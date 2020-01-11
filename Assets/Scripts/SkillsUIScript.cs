2de87d9c7047f0a34f0497fb44dca2f2b09dc415
jankowskipawel <paweljankowski246@gmail.com>
2020-01-11 19:09:17 +0100

    Add number of skillpoints in skillpoints UI, Fix tower Ui button bugs

---
+++ Assets/Scripts/SkillsUIScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillsUIScript : MonoBehaviour
+{
+    public Text SkillPointsText;
+
+    private PlayerScript playerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = FindObjectOfType<PlayerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SkillPointsText.text = $"Skillpoints: {playerScript.skillPoints}";
+    }
+}
